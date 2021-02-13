34595ca6c1d3032b7d728449e62fe59828b7ef59
Wolf De Rechter <wolf.derechter@student.hogent.be>
2021-02-13 15:06:21 +0100

    Dotnet project aanmaken

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_dotnet_g_28.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
