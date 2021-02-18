76cd0dc077e7da9dfc0aaca8f7c4c4bcaa87f896
Nathan Tersago <nathan.tersago@student.hogent.be>
2021-02-18 12:18:52 +0100

    Nieuw project + identity framework

---
+++ 2021-dotnet-g-28/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2021_dotnet_g_28.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
