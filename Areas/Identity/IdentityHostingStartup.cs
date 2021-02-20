0d0ae3f85110cadeede46c65799504ab3fc2b989
Nathan Tersago <nathan.tersago@student.hogent.be>
2021-02-20 15:56:10 +0100

    scaffolded register

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _2021_dotnet_g_28.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(_2021_dotnet_g_28.Areas.Identity.IdentityHostingStartup))]
+namespace _2021_dotnet_g_28.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
