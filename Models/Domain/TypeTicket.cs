bbaf49fbeec14b162668b7b96b7568aa95461efe
Wolf De Rechter <wolf.derechter@student.hogent.be>
2021-02-27 16:03:27 +0100

    fix type tickets

--- Models/Domain/TypeTicket.cs
+++ Models/Domain/TypeTicket.cs
@@ -10,10 +10,10 @@ namespace _2021_dotnet_g_28.Models.Domain
     public enum TypeTicket
     {
         [Display(Name  = "1: productie geïmpacteerd, binnen 2u een oplossing")]
-         type1
-        //[Description("2: productie zal binnen een tijd stil vallen , binnen 4u oplossing")]
-        //type2,
-        //[Description("3: geen productie impact, binnen 3d een antwoord")]
-        //type3
+        type1,
+        [Display(Name = "2: productie zal binnen een tijd stil vallen , binnen 4u oplossing")]
+        type2,
+        [Display(Name = "3: geen productie impact, binnen 3d een antwoord")]
+        type3
     }
 }
