dab689d4c0383114367c425420761eb67703ef9d
Wolf De Rechter <wolf.derechter@student.hogent.be>
2021-03-17 13:43:00 +0100

    Testen folder aanmaken

---
+++ 2021-dotnet-g-28/2021-dotnet-g-28.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace _2021_dotnet_g_28.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
