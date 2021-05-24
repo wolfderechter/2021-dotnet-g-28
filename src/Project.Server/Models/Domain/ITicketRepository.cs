62e9b1a12dd0569fb08914cf4850ae53bba8bc1f
Nathan Tersago <nathan.tersago@student.hogent.be>
2021-05-24 18:45:30 +0200

    fix demo

--- src/Project.Server/Models/Domain/ITicketRepository.cs
+++ src/Project.Server/Models/Domain/ITicketRepository.cs
@@ -8,8 +8,8 @@ namespace _2021_dotnet_g_28.Models.Domain
     public interface ITicketRepository
     {
         Ticket GetBy(int ticketNr);
-        IEnumerable<Ticket> GetByStatusAndType(IEnumerable<TicketEnum.Status> statusses, IEnumerable<TicketEnum.Type> types);
-        IEnumerable<Ticket> GetByContactPersonId(int contactPersonId);
+        IEnumerable<Ticket> GetByStatusAndType(IEnumerable<TicketEnum.Status> statusses, IEnumerable<TicketEnum.Type> types,int companyNr);
+        IEnumerable<Ticket> GetByContactPersonId(int companyNr);
         IEnumerable<Ticket> GetAll();
         void Add(Ticket ticket);
         void SaveChanges();
